ae00209b73b4b9c35057b91f02da3ac32945b7ce
natt17 <marketing20.nat@gmail.com>
2018-12-11 17:26:48 -0500

    funcion de listas

--- src/main.js
+++ src/main.js
@@ -19,3 +19,35 @@ listAttack.addEventListener("click", () => {
   const attackChampions = attackPower(newArr);
   document.write(attackChampions);
 });
+
+let magicPowerFilter = (newArr) => {
+  const arr3 = [];
+  for (var i = 0; i < newArr.length; i++) {
+    const magicChampions = newArr[i];
+    const filterDataMagic = (magicChampions.info.magic);
+    arr3[i] = filterDataMagic;
+    document.write(arr3[i]);
+  }
+}
+const btnListMagic = document.getElementById("btn-magic");
+btnListMagic.addEventListener("click", () => {
+  const newArr3 = Object.values(LOL.data);
+  const functionFilterMagic = magicPowerFilter(newArr3);
+  document.write(functionFilterMagic);
+})
+
+let difficultyPowerFilter = (newArr) => {
+  const arr4 = [];
+  for (var i = 0; i < newArr.length; i++) {
+    const difficultyChampions = newArr[i];
+    const filterDataDifficulty = (diffChampions.info.difficulty);
+    arr4[i] = filterDataDifficulty;
+    document.write(arr4[i]);
+  }
+}
+const btnListDifficulty = document.getElementById("btn-difficulty");
+btnListDifficulty.addEventListener("click", () => {
+  const newArr4 = Object.values(LOL.data);
+  const functionFilterDifficulty = difficultyPowerFilter(newArr4);
+  document.write(functionFilterDifficulty);
+})
