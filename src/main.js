1659f1d946d5e852de814d696c8edc268cfeb967
JanetGM <antaly.146@gmail.com>
2018-12-15 10:40:38 -0500

    funcionalidad de listar con nombres , ataque e imagen

--- src/main.js
+++ src/main.js
@@ -1,21 +1,30 @@
-console.table(LOL.data);
-console.log(Object.entries(LOL.data));
-
-
-
-let attackPower = (newArr) => {
-  const arr = [];
-  for (let i = 0; i < newArr.length; i++) {
-    const champions = newArr[i];
-    const statsChampions = (champions.stats.attackdamage);
-    arr[i]=statsChampions;
-    arr.sort();
-    document.write(arr[i]);
-  }
+//aqui obtengo 134 datos para iniciar el for
+let array=Object.values(LOL.data);
+//aqui los nombres de todos los campeones
+const names=Object.keys(LOL.data);
+//declarando los arrays
+let newArrayNameAttack=[];
+let arrayPower = [];
+const arr=[];
+let templateList='';
+for (let i = 0; i < array.length; i++) {
+  const nameArray = array[i];
+  const championAttack = (nameArray.stats.attackdamage);
+  const championImage = (nameArray.img);
+  const name=(nameArray.name);
+  newArrayNameAttack= name;
+  function listar(name,championAttack,championImage) {
+      this.name = name;
+      this.championAttack = championAttack;
+      this.championImage = championImage;
 }
-const listAttack = document.getElementById('btn-attack');
-listAttack.addEventListener("click", () => {
-  const newArr = Object.values(LOL.data);
-  const attackChampions = attackPower(newArr);
-  document.write(attackChampions);
-});
+listar(name,championAttack,championImage);
+
+  const li =`
+  <li>${name}</li>
+  <li>${championAttack}</li>
+  <img src="${championImage}">`
+  templateList +=li;
+}
+const ulContainer = document.getElementById('container-list');
+ ulContainer.innerHTML = templateList;
